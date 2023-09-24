3a6b987348bf4dfd459cc2ad03b164301236929e
Javier Carrasco <javierdeveloper08@gmail.com>
2023-09-24 17:28:20 -0400

    feat: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ["inclusive-sans"]: ["Inclusive Sans", "sans-serif"],
+      },
+      colors: {
+        background: "#0d1321",
+        dollar: "#38A37F",
+        border: "#b3efb2",
+        text: "#FEFEFE",
+        ["red"]: "#ef233c",
+        ["blue"]: "#2667ff",
+        ["yellow"]: "#ffc15e",
+      },
+      backgroundImage: {
+        gradient:
+          "linear-gradient(transparent, transparent,rgba(255,255,255,0.1))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
